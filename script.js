47764d02f6716bf5af42e381a7c8f8dcc3b4552f
Michał Kwaśniewski <92835884+1NOTME1@users.noreply.github.com>
2024-10-17 12:55:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -27,13 +27,14 @@ document.addEventListener("DOMContentLoaded", function() {
     ],
     "wspierające": [
         "Wiadomość testowa",
+
         "Wiem, że czasami są cięższe dni, ale zawsze możesz na mnie liczyć i zawsze będę przy tobie💞 jeśli nie fizycznie to na pewno psychicznie😊❤️",
 
         "Jestem z ciebie bardzo dumny💗💗 nawet gdy wszyscyci mówią, że niewystarczająco się starasz,ja wiem, że robisz bardzo dużo, i często przez to nie masz czasu dla siebie🙁❤️ ja to widzęi bardzo cie kocham kasia💗❤️",
 
         "Zawsze będę cię wspierał w twoich celach i postanowieniach🌸❤️‍🔥, chce ci dawać wszystko co najlepsze i potrzebne💝❤️",
         "Jesteś silniejsza, niż myślisz 💪",
-        "Zawsze będę Twoim wsparciem, niezależnie od okoliczności 🤍"
+        "Pamiętaj, zależy mi na tobie tak bardzo, że jestem w stanie tego samego dnia wejść w pociągu by sie u ciebie pojawić jesli tylko byśtego potrzebowała😶‍🌫️❤️"
     ]
 };
 
